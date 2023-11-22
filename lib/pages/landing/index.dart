31c6e2dc93eeb6b8a4636fc5ca3dc5c16e5db70b
DESKTOP-8GJUDCB\Tom <kimthien175@gmail.com>
2023-11-22 17:57:35 +0700

    add gif logo to landing page

--- lib/pages/landing/index.dart
+++ lib/pages/landing/index.dart
@@ -7,7 +7,7 @@ class LandingController extends GetxController {
   var locale = Get.deviceLocale.toString().obs; // update when translations loading finish
   String name = "";
 
-  getReady(){
+  getReady() {
     ready.value = true;
   }
 }
@@ -24,24 +24,29 @@ class LandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Obx(() => controller.ready.isFalse
-            ? const Center(child: CircularProgressIndicator())
-            : Center(
-                child: Column(children: [
-                DropdownButton(
-                    value: controller.locale.value,
-                    items: GameTranslations.inst.keys.entries.map((entry) {
-         
-
-                      return DropdownMenuItem<String>(value: entry.key, child: Text(entry.value['displayName']!));
-                    }).toList(),
-                    onChanged: (String? locale) {
-                      // switch locale
-                      List<String> lanRegion = locale!.split('_');
-                      controller.locale.value = locale;
-                      Get.updateLocale(Locale(lanRegion[0], lanRegion[1]));
-                    }),
-                Text('displayName'.tr)
-              ]))));
+        body: Container(
+            decoration: const BoxDecoration(image: DecorationImage(
+              fit: BoxFit.cover,
+              image: AssetImage('assets/background.png'))),
+            child: Obx(() => controller.ready.isFalse
+                ? const Center(child: CircularProgressIndicator())
+                : Center(
+                    child: Column(children: [
+                    Image.asset(
+                      'assets/landing_logo.gif'
+                    ),
+                    DropdownButton(
+                        value: controller.locale.value,
+                        items: GameTranslations.inst.keys.entries.map((entry) {
+                          return DropdownMenuItem<String>(value: entry.key, child: Text(entry.value['displayName']!));
+                        }).toList(),
+                        onChanged: (String? locale) {
+                          // switch locale
+                          List<String> lanRegion = locale!.split('_');
+                          controller.locale.value = locale;
+                          Get.updateLocale(Locale(lanRegion[0], lanRegion[1]));
+                        }),
+                    Text('displayName'.tr)
+                  ])))));
   }
 }
