24fee778fe7ee3f27a6407b3a5c60dcc2aa12a4e
unknown <kimthien175@gmail.com>
2024-06-08 06:23:29 +0700

    temp commit: web

--- lib/pages/home/layouts/web.dart
+++ lib/pages/home/layouts/web.dart
@@ -11,19 +11,50 @@ import 'package:cd_mobile/widgets/logo.dart';
 import 'package:cd_mobile/pages/home/widgets/random_avatars.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:measure_size/measure_size.dart';
+
+class WebController extends GetxController {
+  final RxBool isHeightFit = false.obs;
+  final RxBool isWidthFit = false.obs;
+  void processLayout() {
+    var homeController = Get.find<HomeController>();
+    var mainSize = homeController.mainContentKey.currentContext?.size;
+    var footerSize = homeController.footerKey.currentContext?.size;
+    if (mainSize != null && footerSize != null) {
+      isHeightFit.value = mainSize.height + footerSize.height <= Get.height;
+      isWidthFit.value = mainSize.width + footerSize.width <= Get.width;
+    } else {
+      isHeightFit.value = false;
+      isWidthFit.value = false;
+    }
+  }
+
+  final horizontalScrollController = ScrollController();
+  final verticalScrollController = ScrollController();
+
+  @override
+  void dispose() {
+    horizontalScrollController.dispose();
+    verticalScrollController.dispose();
+    super.dispose();
+  }
+}
 
 // TODO: WHEN USER ZOOM IN OR RESIZE TO SMALLER WIDTH, MAKE FOOTER SECTIONS JUMP DOWN LIKE TRUE MEANING OF WRAP
-class Web extends StatelessWidget {
+class Web extends GetView<WebController> {
   const Web({super.key});
 
   @override
   Widget build(BuildContext context) {
+    Get.put(WebController());
     var homeController = Get.find<HomeController>();
-    var mainContent = IntrinsicWidth(
-        child:
-            Column(key: homeController.mainContentKey, mainAxisSize: MainAxisSize.min, children: [
+    var mainContent =
+        Column(key: homeController.mainContentKey, mainAxisSize: MainAxisSize.min, children: [
       const SizedBox(height: 25),
-      Logo(() => Get.offNamed('/')),
+      Logo(() {
+        // check if private code in url, then clean it
+        Get.parameters = {};
+      }),
       const SizedBox(height: 10),
       const RandomAvatars(),
       const SizedBox(height: 40),
@@ -41,39 +72,110 @@ class Web extends StatelessWidget {
             ],
           )),
       const SizedBox(height: 10)
-    ]));
-    var footer = IntrinsicWidth(
-        child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            key: homeController.footerKey,
-            mainAxisSize: MainAxisSize.min,
-            children: const [Triangle(), Footer()]));
+    ]);
 
-    var horizontalScrollController = ScrollController();
-    var verticalScrollController = ScrollController();
+    var footer = Column(
+      key: homeController.footerKey,
+      mainAxisSize: MainAxisSize.min,
+      children: const [Triangle(), Footer()],
+    );
 
-    // return Obx(() {
-    //   var controller = Get.find<HomeController>();
-    //   if (controller.isFirstFrame.value) {
+    return Obx(() => MeasureSize(
+          onChange: (_) => controller.processLayout(),
+          child: controller.isWidthFit.value
+              ? (controller.isHeightFit.value
+                  ?
+                  // TODO: fit width and height
+                  SingleChildScrollView(
+                      child: SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: Container(
+                              constraints:
+                                  BoxConstraints(minHeight: Get.height, minWidth: Get.width),
+                              child: Stack(
+                                clipBehavior: Clip.none,
+                                children: [mainContent, Positioned(bottom: 0, child: footer)],
+                              ))))
+                  :
+                  // TODO:  fit width but not height
+                  Container())
+              : (controller.isHeightFit.value
+                  ?
+                  // TODO:fit height but not width
+                  Container()
+                  :
+                  // TODO:  fit no height nor width
+                  SingleChildScrollView(
+                      child: SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: Container(
+                              constraints:
+                                  BoxConstraints(minHeight: Get.height, minWidth: Get.width),
+                              child: Stack(
+                                clipBehavior: Clip.none,
+                                children: [mainContent, Positioned(bottom: 0, child: footer)],
+                              ))))),
+        ));
+    // var controller = Get.find<HomeController>();
+    // if (controller.isFit.value) {
+    //   if (_widthController.isFit.value) {
+    //     // fit height and width
+    //     return MeasureSize(
+    //         onChange: (_) {
+    //           controller.processLayout();
+    //           _widthController.processLayout();
+    //         },
+    //         child: SingleChildScrollView(
+    //           scrollDirection: Axis.horizontal,
+    //           child: SingleChildScrollView(
+    //             child:
+    //           )
+    //         ));
+    //   }
+    //   // fit height, not width
+    //   return MeasureSize(
+    //       onChange: (_) {
+    //         controller.processLayout();
+    //         _widthController.processLayout();
+    //       },
+    //       child: Container());
+    // }
+    // if (_widthController.isFit.value) {
+    //   // fit width, not height
+    //   return MeasureSize(
+    //       onChange: (_) {
+    //         controller.processLayout();
+    //         _widthController.processLayout();
+    //       },
+    //       child: Container());
+    // }
+    // // not fit width nor height
+    // return MeasureSize(
+    //     onChange: (_) {
+    //       controller.processLayout();
+    //       _widthController.processLayout();
+    //     },
+    //     child: Container());
+
     // TODO: vertical scrollbar has to stay in right of the screen, not the right of the widget
-    return Scrollbar(
-        thumbVisibility: true,
-        trackVisibility: true,
-        scrollbarOrientation: ScrollbarOrientation.right,
-        controller: verticalScrollController,
-        child: Scrollbar(
-            thumbVisibility: true,
-            trackVisibility: true,
-            controller: horizontalScrollController,
-            child: SingleChildScrollView(
-                controller: horizontalScrollController,
-                scrollDirection: Axis.horizontal,
-                child: SingleChildScrollView(
-                    controller: verticalScrollController,
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [mainContent, footer],
-                    )))));
+    // return Scrollbar(
+    //     thumbVisibility: true,
+    //     trackVisibility: true,
+    //     scrollbarOrientation: ScrollbarOrientation.right,
+    //     controller: verticalScrollController,
+    //     child: Scrollbar(
+    //         thumbVisibility: true,
+    //         trackVisibility: true,
+    //         controller: horizontalScrollController,
+    //         child: SingleChildScrollView(
+    //             controller: horizontalScrollController,
+    //             scrollDirection: Axis.horizontal,
+    //             child: SingleChildScrollView(
+    //                 controller: verticalScrollController,
+    //                 child: Column(
+    //                   mainAxisSize: MainAxisSize.min,
+    //                   children: [mainContent, footer],
+    //                 )))));
     // } else if (homeController.isFit.value) {
     //   return Scrollbar(
     //       controller: horizontalScrollController,
