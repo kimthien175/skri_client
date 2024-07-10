1894b77154a8078279b7cc95a37d98ac295d1a10
kimthien175 <kimthien175@gmail.com>
2024-07-11 03:34:18 +0700

    dialog blur background done

--- lib/widgets/dialog.dart
+++ lib/widgets/dialog.dart
@@ -67,71 +67,73 @@ class _GameDialogState extends State<GameDialog> with SingleTickerProviderStateM
     return FadeTransition(
         opacity: bgAnimation,
         child: Container(
-            alignment: Alignment.center,
-            color: const Color.fromRGBO(0, 0, 0, 0.55),
-            height: context.height,
-            width: context.width,
-            child: SlideTransition(
+          alignment: Alignment.center,
+          color: const Color.fromRGBO(0, 0, 0, 0.55),
+          height: context.height,
+          width: context.width,
+          child: SlideTransition(
               position: dialogAnimation,
               child: DefaultTextStyle(
                   style: const TextStyle(fontFamily: 'Nunito-var'),
-                  child: BackdropFilter(
-                      filter: ImageFilter.blur(sigmaX: 4, sigmaY: 4),
-                      child: Stack(children: [
-                        Container(
-                            decoration: BoxDecoration(
-                                color: const Color.fromRGBO(12, 44, 150, 0.75),
-                                boxShadow: const [
-                                  BoxShadow(color: Color.fromRGBO(0, 0, 0, 0.15), blurRadius: 50)
-                                ],
-                                borderRadius: BorderRadius.circular(10)),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                // title
-                                Padding(
-                                    padding:
-                                        const EdgeInsets.only(top: 13.5, left: 13.5, right: 40),
-                                    child: Text(widget.title,
-                                        style: const TextStyle(
-                                            color: Colors.white,
-                                            fontSize: 27,
-                                            fontVariations: [FontVariation('wght', 750)]))),
-                                // content
-                                Padding(
-                                    padding: const EdgeInsets.only(
-                                        top: 7.5, left: 15, right: 15, bottom: 15),
-                                    child: widget.content)
-                              ],
-                            )),
-                        Positioned(
-                            top: 0,
-                            right: 8,
-                            child: GestureDetector(
-                                onTap: () {
-                                  animController.reverse().then((_) {
-                                    widget.hide();
-                                  });
-                                },
-                                child: StatefulBuilder(
-                                    builder: (ct, setState) => MouseRegion(
-                                        onEnter: (_) => setState(() {
-                                              isHover = true;
-                                            }),
-                                        onExit: (_) => setState(() {
-                                              isHover = false;
-                                            }),
-                                        child: Text('×',
-                                            style: TextStyle(
-                                              fontSize: 40.5,
-                                              color: isHover
-                                                  ? Colors.white
-                                                  : const Color.fromRGBO(170, 170, 170, 1),
-                                              height: 1,
-                                              fontVariations: const [FontVariation('wght', 850)],
-                                            ))))))
-                      ]))),
-            )));
+                  child: Stack(children: [
+                    ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: BackdropFilter(
+                            filter: ImageFilter.blur(sigmaX: 4, sigmaY: 4),
+                            child: Container(
+                                decoration: const BoxDecoration(
+                                  color: Color.fromRGBO(12, 44, 150, 0.75),
+                                  boxShadow: [
+                                    BoxShadow(color: Color.fromRGBO(0, 0, 0, 0.15), blurRadius: 50)
+                                  ],
+                                ),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    // title
+                                    Padding(
+                                        padding:
+                                            const EdgeInsets.only(top: 13.5, left: 13.5, right: 40),
+                                        child: Text(widget.title,
+                                            style: const TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 27,
+                                                fontVariations: [FontVariation('wght', 750)]))),
+                                    // content
+                                    Padding(
+                                        padding: const EdgeInsets.only(
+                                            top: 7.5, left: 15, right: 15, bottom: 15),
+                                        child: widget.content)
+                                  ],
+                                )))),
+                    Positioned(
+                        top: 0,
+                        right: 8,
+                        child: GestureDetector(
+                            onTap: () {
+                              animController.reverse().then((_) {
+                                widget.hide();
+                              });
+                            },
+                            child: StatefulBuilder(
+                                builder: (ct, setState) => MouseRegion(
+                                    onEnter: (_) => setState(() {
+                                          isHover = true;
+                                        }),
+                                    onExit: (_) => setState(() {
+                                          isHover = false;
+                                        }),
+                                    child: Text('×',
+                                        style: TextStyle(
+                                          fontSize: 40.5,
+                                          color: isHover
+                                              ? Colors.white
+                                              : const Color.fromRGBO(170, 170, 170, 1),
+                                          height: 1,
+                                          fontVariations: const [FontVariation('wght', 850)],
+                                        ))))))
+                  ]))),
+        ));
   }
 }
