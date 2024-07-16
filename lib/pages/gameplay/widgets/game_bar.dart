1a04d5dbcdd6298abc854e5d6737f9450d9cff1f
DESKTOP-VCGON47\Tom <kimthien175@gmail.com>
2024-07-16 16:42:47 +0700

    done game clock jiggle animation, remaining the problem as TODO: if all player guess right before time out, disable animtion

--- lib/pages/gameplay/widgets/game_bar.dart
+++ lib/pages/gameplay/widgets/game_bar.dart
@@ -56,44 +56,31 @@ class GameBar extends StatelessWidget {
                   .builder
                   .initWithShadow(filterQuality: FilterQuality.none, height: 42, width: 42)))
     ]);
-
-    // Container(
-    //     width: 300,
-    //     alignment: Alignment.centerRight,
-    //     child: AnimatedButtonBuilder(
-    //             child: namedGifs('settings')
-    //                 .builder
-    //                 .initWithShadow()
-    //                 .doFitSize(height: 48, width: 48),
-    //             decorators: [AnimatedButtonOpacityDecorator(minOpacity: 0.85)],
-    //             onTap: () {
-
-    //             })
-    //         .child)
   }
 }
 
-// TODO: ADD EFFECT WHEN ALMOST RUN OUT OF TIME, start at 9, as soon as the new number shows, the animation starts
-
+/// Count down, start animation when time smaller than 10
 class GameClock extends GetView<GameClockController> {
   const GameClock({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ScaleTransition(
-        scale: controller.scaleAnimation,
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            GifManager.inst.misc('clock').builder.initWithShadow(width: 64, height: 64),
-            Positioned(
-                top: 20,
-                child: Obx(() => Text(
-                    controller.remainingTime.value
-                        .toString(), //Game.inst.remainingTime.seconds.value.toString(),
-                    style: const TextStyle(fontSize: 22, fontWeight: FontWeight.w800))))
-          ],
-        ));
+    return RotationTransition(
+        turns: controller.rotateAnimation,
+        child: ScaleTransition(
+            scale: controller.scaleAnimation,
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                GifManager.inst.misc('clock').builder.initWithShadow(width: 64, height: 64),
+                Positioned(
+                    top: 20,
+                    child: Obx(() => Text(
+                        controller.remainingTime.value
+                            .toString(), //Game.inst.remainingTime.seconds.value.toString(),
+                        style: const TextStyle(fontSize: 22, fontWeight: FontWeight.w800))))
+              ],
+            )));
   }
 }
 
@@ -101,47 +88,58 @@ class GameClockController extends GetxController with GetSingleTickerProviderSta
   @override
   onInit() {
     super.onInit();
+
     controller = AnimationController(
       vsync: this,
       duration: const Duration(milliseconds: 50),
       reverseDuration: const Duration(milliseconds: 250),
     );
 
-    scaleAnimation = CurvedAnimation(parent: controller, curve: Curves.easeInOut)
-        .drive(Tween<double>(begin: 1.0, end: scale));
+    curvedAnimation = CurvedAnimation(parent: controller, curve: Curves.easeInOut);
+
+    scaleAnimation = curvedAnimation.drive(Tween<double>(begin: 1.0, end: 1.4));
+
+    rotateTween = Tween<double>(begin: 0, end: turnsEnd);
+    rotateAnimation = curvedAnimation.drive(rotateTween);
 
     timer = Timer.periodic(const Duration(seconds: 1), decreaseSeconds);
   }
 
   late final Timer timer;
 
-  void decreaseSeconds(Timer timer) {
+  Future<void> decreaseSeconds(Timer timer) async {
     if (remainingTime.value == 0) {
       timer.cancel();
       return;
     }
+
     remainingTime--;
-    controller.forward().then((value) {
-      controller.reverse();
-    });
+    if (remainingTime.value >= 10 // TODO: || Game.inst.allCorrectBeforeTimeEnd
+        ) return;
+
+    await controller.forward();
+    await controller.reverse();
+
+    // reverse rotate direction
+    rotateTween.end = turnsEnd;
   }
 
   var remainingTime = 20.obs;
 
   late final AnimationController controller;
+  late final CurvedAnimation curvedAnimation;
 
   late final Animation<double> scaleAnimation;
 
-//   late final Animation<double> rotationAnimation = Tween<double>(
-//     begin: 0,
-//     end: 1,
-//   ).animate(controller);
-
-  static const double scale = 1.4;
+  late final Animation<double> rotateAnimation;
+  late final Tween<double> rotateTween;
+  double get turnsEnd => (remainingTime.value % 2 == 0) ? 0.125 : -0.125;
 
   @override
   void onClose() {
+    curvedAnimation.dispose();
     controller.dispose();
+
     super.onClose();
   }
 }
